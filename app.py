081f5b2e03f9ced3518582145ff555cc9c9c0ead
Qaun on lynux <23259@wc.school.nz>
2026-09-27 00:42:16 +1200

    Sometimes i sit and wonder about the amount of bullshit i have to add to make a website work

--- app.py
+++ app.py
@@ -1,40 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
-DATABASE = "yugioh.db"
+
+DATABASE = "terraria.db"
 
 
 def create_connection(db_file):
-    """
-    Creates a connection to the database
-    :parameter db_file - the name of the file
-    :returns   connection - a connection to the database
-    """
     try:
         connection = sqlite3.connect(db_file)
         return connection
     except Error as e:
         print(e)
+
     return None
 
 
 def run_query(query):
     con = create_connection(DATABASE)
+
+    if con is None:
+        return []
+
     cur = con.cursor()
     cur.execute(query)
+
     results = cur.fetchall()
+
     con.close()
+
     return results
 
 
-@app.route('/')
+@app.route("/")
 def render_home():
-    cards = run_query("SELECT * FROM tbl_cards")
+    card = run_query("SELECT * FROM weapons WHERE ID IN (3,4,20)")
+
+    return render_template("home_card.html", cards=card)
+
+
+@app.route("/data")
+def render_data():
+    cards = run_query("SELECT * FROM weapons")
 
     return render_template("cards.html", cards=cards)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
